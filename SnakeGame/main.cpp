7f8b2631175708fb9fa4a9e4eead1b1621840f7c
Ivan Figueroa <Ivanfiggomez@gmail.com>
2024-11-16 16:02:43 -0500

    Updated main.cpp
    Finished the functions for drawing the board and generating food

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-
-
+#include <random>
 const int width = 50;
 const int height = 25;
+
+const char wall = '#';
+const char food = '*';
+const char body = 'O';
+const char space = ' ';
 //function declaratons
 void draw_board(char board[height][width]); //this function is to draw the gameboard.
-void generate_food() //this function will take the gameboard array as input and return the gameboard array with food.
+void generate_food(char board[height][width]); //this function will take the gameboard array as input and return the gameboard array with food.
 int main() {
 	
-	char wall = '#';
-	char food = '*';
-	char body = 'O';
-	char space = ' ';
-
 	//create 2d array for the game board
 	char gameBoard[height][width];
 
@@ -29,7 +28,8 @@ int main() {
 			}
 		}
 	}
-	//call the function to generate food here,
+	//generate food
+	generate_food(gameBoard);
 
 	//infinite loop for drawing game board, processing input, updating game logic
 	while (true) {
@@ -62,15 +62,26 @@ void draw_board(char board[height][width]) {
 
 }
 
-void generate_good(char board[height][width]) {
-	// we need to generate food that is proportionate to the size of the board
+void generate_food(char board[height][width]) {
+	//we need to generate food that is proportionate to the size of the board
 	//the size of the board is height * width, let's do (height * width) / 20 
-	//			to represent the total amount of starting food
+	//to represent the total amount of starting food
 
 	int board_area = height * width;
 	int food_amt = (height * width) / 20;
 
-	//we will use a set of tuples to store food coordinates,
-	
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	//uniform distribution range
+	std::uniform_int_distribution<> height_dis(1, height-1);
+	std::uniform_int_distribution<> width_dis(1, width-1);
+
+	//for loop to update board
+	for (int i = 1; i <= food_amt; i++) {
+		int x = height_dis(gen);
+		int y = width_dis(gen);
+		board[x][y] = food;
+	}
 }
 
